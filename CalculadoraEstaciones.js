7587c09f90c291e04aac55877625d4ffc3191212
sharonalvarado <mitsy.alvarado@galileo.com>
2023-02-12 16:57:16 -0600

    Esta es la actividad 2 Calculadora Estaciones

---
+++ CalculadoraEstaciones.js
@@ -0,0 +1,37 @@
+
+var Mes = Number(prompt("Ingrese un Número del 1 al 12"));
+
+switch(Mes){
+  case 1:
+alert ("Invierno");alert ("Mitsy Alvarado, Carné 23002603")
+break
+case 2:alert("Invierno");alert ("Mitsy Alvarado, Carné 23002603")
+break
+case 3: alert("Primavera"); alert ("Mitsy Alvarado, Carné 23002603")
+break
+case 4: alert("Primavera");alert ("Mitsy Alvarado, Carné 23002603")
+break
+case 5: alert ("Primavera");alert ("Mitsy Alvarado, Carné 23002603")
+break
+case 6: alert ("Verano");alert ("Mitsy Alvarado, Carné 23002603")
+break
+case 7: alert ("Verano");alert ("Mitsy Alvarado, Carné 23002603")
+break
+case 8: alert("Verano");alert ("Mitsy Alvarado, Carné 23002603")
+break
+case 9: alert ("Otoño");alert ("Mitsy Alvarado, Carné 23002603")
+break
+case 10: alert ("Otoño");alert ("Mitsy Alvarado, Carné 23002603")
+break
+case 11: alert ("Otoño");alert ("Mitsy Alvarado, Carné 23002603")
+break
+case 12: alert ("Invierno");alert ("Mitsy Alvarado, Carné 23002603")
+break
+default:alert("No es un mes")}
+
+
+
+
+
+
+
